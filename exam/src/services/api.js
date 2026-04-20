f1e24ff0ce764fcb4937d3e5c833302cfa44b3b0
Kona Ajay <konaajay0022@gmail.com>
2026-04-20 12:05:00 +0530

    fix dockerfile path

--- exam/src/services/api.js
+++ exam/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3131',
+    baseURL: 'https://exammodulemcq.onrender.com',
     timeout: 120000 // 120 seconds for large bulk uploads
 });
 
